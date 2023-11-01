ff7a0ec5aaee21f0e603aed3e379785cb1705691
GermandaS <germandasilveira@gmail.com>
2023-11-01 19:47:53 -0300

    js
    comienzo

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
 // Mi código JavaScript:
+const Filtrar= document.querySelector(".filtrarAuto")
+
+
+Filtrar.addEventListener("click",function(){
+  fetch("https://ha-front-api-proyecto-final.vercel.app/cars" + search.value)
+
+  .then(function(res){
+    return res.json();
+
+  })
+  .then(function(res){
+    
+
+  })
+  .catch(function(err){
+
+  })
+})
