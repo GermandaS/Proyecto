7bda63b3ff18f612b7db43cead9649528d7a8324
Atei-Yu <148918192+Atei-Yu@users.noreply.github.com>
2023-11-03 21:47:13 -0300

    update

--- js/app.js
+++ js/app.js
@@ -17,18 +17,44 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 });
 
-const imgElement = document.querySelector(".auto");
-
-// Realiza una solicitud HTTP para obtener los datos desde la URL
 fetch("https://ha-front-api-proyecto-final.vercel.app/cars")
-  .then((response) => response.json())
-  .then((data) => {
-    // Asegúrate de que el JSON tenga la estructura adecuada
-    if (data.car && data.car.image) {
-      // Asigna la ruta de la imagen al atributo src del elemento img
-      imgElement.src = data.car.image;
-    } else {
-      console.error("La estructura del JSON no es la esperada.");
+  .then(function (res) {
+    return res.json();
+  })
+  .then(function (cars) {
+    for (let car of cars) {
+      let carElement = `
+          <div class="row">
+              <div class="col-12 col-lg-4 mb-3">
+                  <img class="auto img-fluid" src="${car.image}" alt="" />
+              </div>
+              <div class="col-12 col-lg-8">
+                  <h4>${car.model}</h4>
+                  <div class="d-flex justify-content-end">
+                      <h5>
+                          ${car.year} | ${car.price_usd} |
+                          <i class="bi bi-star-fill text-warning"></i>
+                          <i class="bi bi-star-fill text-warning"></i>
+                          <i class="bi bi-star-fill text-warning"></i>
+                          <i class="bi bi-star-fill text-warning"></i>
+                          <i class="bi bi-star text-warning"></i>
+                      </h5>
+                  </div>
+                  <p>${car.description}</p>
+                  <button type="submit" id="shopingCart">
+                      <i class="bi bi-cart3"></i>
+                      Comprar
+                  </button>
+                  <button type="submit" id="infoCart">
+                      <i class="bi bi-plus-square"></i> Más información
+                  </button>
+                  <button type="submit" id="shareCart">
+                      <i class="bi bi-box-arrow-up-right"></i> Compartir
+                  </button>
+              </div>
+          </div>
+      `;
+      document.insertAdjacentHTML("beforeend", carElement);
     }
   })
   .catch((error) => {
